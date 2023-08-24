def795c44d5f2908a8b25ed2cf4afb949a8e383d
Michael Lopez <105012275+Aluacard-Tepes@users.noreply.github.com>
2023-08-24 11:00:20 -0500

    Update Unión_Excel.py

--- Unión_Excel.py
+++ Unión_Excel.py
@@ -1,23 +1,30 @@
+# Importación de las librerías necesarias
 import pandas as pd
 import glob
 import openpyxl
 from tkinter import filedialog
 
+# Solicitar al usuario que seleccione una carpeta
 folder_selected = filedialog.askdirectory()
 
+# Verificar si no se seleccionó ninguna carpeta y salir del programa
 if not folder_selected: 
-    (exit)
+    (exit) # Salir del programa
 
+# Encontrar todos los archivos Excel (.xlsx) en la carpeta seleccionada
 file_list = glob.glob(folder_selected + "/*.xlsx")
 
+# Crear una lista vacía para almacenar DataFrames de archivos Excel
 excel_list = []
 
+# Iterar a través de la lista de archivos y leer cada archivo Excel en un DataFrame
 for file in file_list:
     excel_list.append(pd.read_excel(file))
     
-    
+# Crear un DataFrame vacío para almacenar la consolidación    
 excel_merged = pd.DataFrame()
 
+# Iterar a través de la lista de DataFrames y concatenarlos en el DataFrame consolidado
 for excel_file in excel_list:
     excel_merged = pd.concat([excel_merged, pd.DataFrame.from_records(excel_file)], ignore_index= False)
     
